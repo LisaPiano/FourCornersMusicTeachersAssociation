edddaefe854b9ad0ce79f1bd71370a77e364cf14
lwalte22 <lwalte22@wgu.edu>
2026-01-21 10:17:00 -0500

    completely fix hamburger menu in smaller screens

--- script.js
+++ script.js
@@ -2,27 +2,6 @@
 
 
 
- /* Toggle between adding and removing the "change" and "done" classes to the "hide" element when the user clicks on the hamburger*/
-//  This allows the nav links to appear and disappear on the click
-
-
-    // let hamburger = document.querySelector("#fa");
-    // let hide = document.querySelectorAll(".hide");
-    // let change = document.querySelectorAll(".change");
-    
-    //  hamburger.addEventListener("click", () => { 
-
-    //     for(let num = 0; num < change.length; num++) {
-    //         if (hide[num].classList.contains("change")) {
-    //             hide[num].classList.remove("change");
-    //             change[num].classList +=(" done");
-    //         } else {
-    //             hide[num].classList.remove("done");
-    //             change[num].classList +=(" change");
-    //         }
-    //     }  
-    // });
-    
     
   /* =========================================
    1. HEADER OVERLAY FADE-IN
@@ -35,29 +14,17 @@ window.addEventListener("load", function () {
 });
 
 
-
-
-
-/* =========================================
-   2. HAMBURGER MENU TOGGLE
+  /* =========================================
+   2. 
    ========================================= */
 
-const hamburger = document.querySelector("#fa");
-const hide = document.querySelectorAll(".hide");
-const change = document.querySelectorAll(".change");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-if (hamburger) {
-    hamburger.addEventListener("click", () => {
-        for (let i = 0; i < change.length; i++) {
-            if (hide[i].classList.contains("change")) {
-                hide[i].classList.remove("change");
-                change[i].classList.add("done");
-            } else {
-                hide[i].classList.remove("done");
-                change[i].classList.add("change");
-            }
-        }
-    });
-}
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("open");
+});
+
 
        
